1adf14a6127285c4fb21079746ae3beaac94e600
Petko Petkov <petkozlatilov1234@gmail.com>
2024-05-01 21:46:33 +0300

    Add Home component with NavBarHome

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css'
+import NavBarHome from '../../Components/NavBarHome/NavBarHome'
+
+const Home = () => {
+  return (
+    <div>
+        <NavBarHome />
+    </div>
+  )
+}
+
+export default Home
