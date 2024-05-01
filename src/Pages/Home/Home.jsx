5fd51faf17c90df6010e213d2b506939f81e1e18
Petko Petkov <petkozlatilov1234@gmail.com>
2024-05-01 22:48:53 +0300

    Add HomeHeader component to Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Home.css'
 import NavBarHome from '../../Components/NavBarHome/NavBarHome'
+import HomeHeader from '../../Components/HomeHeader/HomeHeader'
 
 const Home = () => {
   return (
     <div>
         <NavBarHome />
+        <HomeHeader />
     </div>
   )
 }
