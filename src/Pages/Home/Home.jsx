5c17b5a19a675e04fa85cbbe021f16b2eb791d6a
Petko Petkov <petkozlatilov1234@gmail.com>
2024-05-01 23:38:48 +0300

    Add home-posts grid layout to Home.css and update Home.jsx to include AllPosts component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
-import './Home.css'
-import NavBarHome from '../../Components/NavBarHome/NavBarHome'
-import HomeHeader from '../../Components/HomeHeader/HomeHeader'
+import React from "react";
+import "./Home.css";
+import NavBarHome from "../../Components/NavBarHome/NavBarHome";
+import HomeHeader from "../../Components/HomeHeader/HomeHeader";
+import AllPosts from "../../Components/AllPosts/AllPosts";
+import Footer from "../../Components/Footer/Footer";
 
 const Home = () => {
   return (
     <div>
-        <NavBarHome />
-        <HomeHeader />
-    </div>
-  )
-}
+      <NavBarHome />
+      <HomeHeader />
 
-export default Home
+      <div className="home-posts">
+        <h2>Top posts</h2> 
+        <h2>Recent posts</h2>
+        <AllPosts home="true" order="top" />
+        <AllPosts home="true" />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
