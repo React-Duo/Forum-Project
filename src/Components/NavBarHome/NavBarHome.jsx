e719fbb1593965091267c2f040dcd66eab6548a1
Petko Petkov <petkozlatilov1234@gmail.com>
2024-05-02 10:16:12 +0300

    Update NavBarHome component to conditionally render login buttons based on logged state

--- src/Components/NavBarHome/NavBarHome.jsx
+++ src/Components/NavBarHome/NavBarHome.jsx
@@ -2,20 +2,28 @@ import React from "react";
 import "./NavBarHome.css";
 import { assets } from "../../assets/assets";
 
-const NavBarHome = () => {
+const NavBarHome = ({logged}) => {
   return (
     <div className="NavBarHome">
       <div>
         <img src={assets.logo} alt="Test Image" />
       </div>
-      <h1>Welcome to our community<span></span></h1>
-      <div className="login-buttons-home">
-        <button id="login-btn" >Login</button>
-        <button id="register-btn">Register</button>
-        <button id="logout-btn">Log out</button>
-        <button id="users-btn">Users</button>
-        <img id="profileImg" src={assets.profile}></img>
-      </div>
+      <h1>
+        Welcome to our community<span></span>
+      </h1>
+
+      {logged === "false" ? (
+        <div className="login-buttons-home">
+          <button id="login-btn">Login</button>
+          <button id="register-btn">Register</button>
+        </div>
+      ) : (
+        <div className="login-buttons-home">
+          <button id="logout-btn">Log out</button>
+          <button id="users-btn">Users</button>
+          <img id="profileImg" src={assets.profile}></img>
+        </div>
+      )}
     </div>
   );
 };
