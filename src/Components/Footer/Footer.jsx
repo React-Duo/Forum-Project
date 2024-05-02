ca2504e9d10849ddecc682d05ab8e4beea747f78
Petko Petkov <petkozlatilov1234@gmail.com>
2024-05-02 15:59:56 +0300

    Update Footer component styles and structure

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './Footer.css'
+import { assets } from "../../assets/assets";
 
 const Footer = () => {
   return (
-    <div className='Footer'>Footer</div>
+    <div className='Footer'>
+      <div>
+        <img src={assets.logo} alt='Test Image' />
+      </div>
+      <div>
+        <ul className='servicesFooter'>
+          <li>About</li>
+          <li>Services</li>
+          <li>Blog</li>
+          <li>Contacts</li>
+        </ul>
+      </div>
+      <div className='copyrigth'>
+        <p>Copyright © 2024  </p>
+        <p>Powered by React Duo</p>
+      </div>
+    </div>
   )
 }
 
