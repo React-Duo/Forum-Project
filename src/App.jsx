485f14da66dbe5e16862ec60beaf05eb9d72f649
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-05-02 12:19:15 +0300

    Added Context and Routing to main App component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AuthContext from './Context/AuthContext';
 import "./App.css";
-import Home from "./Pages/Home/Home";
+import Home from "./Views/Home/Home";
+import Login from './Views/Login/Login';
+import Register from './Views/Register/Register';
+import Posts from './Views/Posts/Posts';
+import SinglePost from './Views/SinglePost/SinglePost';
+import CreatePost from './Views/CreatePost/CreatePost';
+import Users from './Views/Users/Users';
+import Profile from './Views/Profile/Profile';
+import Authenticated from './hoc/Authenticated/Authenticated';
+import NotFound from './Views/NotFound/NotFound';
 
 function App() {
+  const[authValue, setAuthValue] = useState(false);
+
   return (
     <>
-      <Home />
+      <BrowserRouter>
+        <AuthContext.Provider value={{isLoggedIn: authValue, setLoginState: setAuthValue}}>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/login" element={<Login />}/>
+          <Route path="/register" element={<Register />}/>
+          <Route path="/posts" element={<Authenticated><Posts /></Authenticated>}/>
+          <Route path="/posts/:id" element={<Authenticated><SinglePost /></Authenticated>}/>
+          <Route path="/create-post" element={<Authenticated><CreatePost /></Authenticated>}/>
+          <Route path="/users" element={<Authenticated><Users /></Authenticated>}/>
+          <Route path="/profile" element={<Authenticated><Profile /></Authenticated>}/>
+          <Route path="*" element={<NotFound />}/>
+        </Routes>
+        </AuthContext.Provider>
+      </BrowserRouter>
     </>
   );
 }
