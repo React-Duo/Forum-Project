5f136a50d994553ed25cf05b9646ca54898a3b0c
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-05-03 14:13:18 +0300

    added register form to Register.jsx

--- src/Views/Register/Register.jsx
+++ src/Views/Register/Register.jsx
@@ -2,6 +2,20 @@ const Register = () => {
 
 
 
+
+
+
+
+    return (
+        <div className="register-form">
+            <label htmlFor="first-name"></label>First Name <input type="text" name="first-name" /> <br />
+            <label htmlFor="last-name">Last Name </label><input type="text" name="last-name" /> <br />
+            <label htmlFor="email">Email address </label><input type="email" name="email" /> <br />
+            <label htmlFor="username">Username </label><input type="text" name="username"/> <br />
+            <label htmlFor="password">Password </label><input type="password" name="password"/>
+        </div>
+    )
+
 }
 
 export default Register;
