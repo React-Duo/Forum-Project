074037445abbc7796cd62939106693b1917803e7
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-05-03 22:37:57 +0300

    wrapped registerUser() in try-catch block

--- src/service/authentication-service.js
+++ src/service/authentication-service.js
@@ -1,8 +1,13 @@
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 import { app } from '../config/firebase-config.js';
 
-export const registerUser = async (email, password) => {
-    const authObject = getAuth(app);
-    await createUserWithEmailAndPassword(authObject, email, password);
+export const registerUser = async (emailAddress, password) => {
+    try {
+        const authObject = getAuth(app);
+        const userCredentials = await createUserWithEmailAndPassword(authObject, emailAddress, password);
+        return userCredentials;
+    } catch (error) {
+        return error.message;
+    }
 }
 
