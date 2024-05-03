3b7ccac880843a2a2f4e4088c655b5a9c4c88bf5
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-05-03 16:43:08 +0300

    added registerUser() fn to authentication-service.js to add users in Firebase Auth service.

--- src/service/authentication-service.js
+++ src/service/authentication-service.js
@@ -1,5 +1,8 @@
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth' ;
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { app } from '../config/firebase-config.js';
 
-
-
+export const registerUser = async (email, password) => {
+    const authObject = getAuth(app);
+    await createUserWithEmailAndPassword(authObject, email, password);
+}
 
