92be310ae4e95eefc85232f3f26ac3036746e4b7
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-05-03 13:19:46 +0300

    moved out firebase configuration from request-service.js

--- src/service/request-service.js
+++ src/service/request-service.js
@@ -1,35 +1,15 @@
-import { initializeApp } from "firebase/app";
-import { getDatabase, ref, push, get, set, update, query, equalTo, orderByChild, orderByKey, goOnline, goOffline } from "firebase/database";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyB5oOxHK0BHyFKt-v8K5MWVyoNRu_UxA7Y",
-    authDomain: "reactduo-forumproject.firebaseapp.com",
-    databaseURL: "https://reactduo-forumproject-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "reactduo-forumproject",
-    storageBucket: "reactduo-forumproject.appspot.com",
-    messagingSenderId: "815156986137",
-    appId: "1:815156986137:web:56e7e5a8cc0e1263b04588",
-    measurementId: "G-PEX8JDPXWH"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-// Initialize Realtime Database and get a reference to the service
-const database = getDatabase(app);
+import database from '../config/firebase-config';
+import { ref, push, get, set, update, query, equalTo, orderByChild, orderByKey, goOnline, goOffline } from "firebase/database";
 
 const getUsers = async () => {
-
     goOnline(database);
-
     try {
-        const snapshot = await get(ref(database, 'comments/1'));
+        const snapshot = await get(ref(database, 'users'));
         if (snapshot.exists()) {
             return snapshot.val();
         } else {
             throw new Error('Data not found!');
         }
-
     } catch (error) {
         return error.message;
     } finally {
