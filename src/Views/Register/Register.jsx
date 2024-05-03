6f98e5799e6ee80b64bb5ec33d59277bddbc0eb3
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-05-03 19:54:44 +0300

    added validation and prevented default behaviour (page reload) to register() fn in Register.jsx

--- src/Views/Register/Register.jsx
+++ src/Views/Register/Register.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react';
-import { checkIfUserExists } from '../../service/request-service.js';
+import { checkIfUserExists, createUser } from '../../service/request-service.js';
 import { registerUser } from '../../service/authentication-service.js';
 import './Register.css';
+
+const MIN_CHAR_LENGTH = 4;
+const MAX_CHAR_LENGTH = 32;
 
 const Register = () => {
     const [userExists, setUserExists] = useState(false);
 
     const getFormData = () => {
-
         const firstName = document.querySelector('#first-name').value;
         const lastName = document.querySelector('#last-name').value;
         const emailAddress = document.querySelector('#email').value;
@@ -18,19 +20,41 @@ const Register = () => {
     }
 
 
-    const register = async () => {
+    const register = async (event) => {
+
+        event.preventDefault();
+
         const formData = getFormData();
-        console.log(formData);
+
+        if (formData.firstName.length < MIN_CHAR_LENGTH || formData.firstName.length > MAX_CHAR_LENGTH) {
+            alert(`First name must be between ${MIN_CHAR_LENGTH} and ${MAX_CHAR_LENGTH} characters long.`); 
+            return;
+        }
+
+        if (formData.lastName.length < MIN_CHAR_LENGTH || formData.lastName.length > MAX_CHAR_LENGTH) {
+            alert(`Last name must be between ${MIN_CHAR_LENGTH} and ${MAX_CHAR_LENGTH} characters long.`); 
+            return;
+        }
+
+        const emailValidationRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+ 
+        if (!emailValidationRegex.test(formData.emailAddress)) {
+            alert(`${formData.emailAddress} is not a valid email address.`);
+            return;
+        }
+
+
 
         const snapshot = await checkIfUserExists(formData.username);
-        
+
         if (snapshot.exists()) {
             setUserExists(true);
             return;
         }
 
-        registerUser(formData.emailAddress, formData.password);
+        await registerUser(formData.emailAddress, formData.password);
 
+        createUser(formData);
 
         
 
@@ -47,14 +71,14 @@ const Register = () => {
     }
 
     return (
-        <div className="register-form">
-            <span><label htmlFor="first-name">First Name </label><input type="text" name="first-name" id='first-name'/></span> <br />
-            <span><label htmlFor="last-name">Last Name </label><input type="text" name="last-name" id='last-name'/></span> <br />
-            <span><label htmlFor="email">Email address </label><input type="email" name="email" id='email'/></span> <br />
+        <form onSubmit={register} className="register-form">
+            <span><label htmlFor="first-name">First Name </label><input type="text" name="first-name" id='first-name' required /></span> <br />
+            <span><label htmlFor="last-name">Last Name </label><input type="text" name="last-name" id='last-name' required /></span> <br />
+            <span><label htmlFor="email">Email address </label><input type="email" name="email" id='email' required /></span> <br />
             <span><label htmlFor="username">Username </label><input type="text" name="username" id='username'/></span> <br />
-            <span><label htmlFor="password">Password </label><input type="password" name="password" id='password'/></span> <br />
-            <button onClick={register}>Register</button>
-        </div>
+            <span><label htmlFor="password">Password </label><input type="password" name="password" id='password' required /></span> <br />
+            <button type="submit">Register</button>
+        </form>
     )
 
 }
