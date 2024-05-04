66faed688fee80a1be08b59fa1aa4b7989e4e242
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-05-04 18:43:52 +0300

    implemented the Authenticated hoc component to guard Posts / Single Post / Create Post / Users / Profile views

--- src/hoc/Authenticated/Authenticated.jsx
+++ src/hoc/Authenticated/Authenticated.jsx
@@ -1,8 +1,20 @@
-const Authenticated = () => {
+import { useContext } from 'react';
+import AuthContext from '../../Context/AuthContext';
+import './Authenticated.css';
 
+const Authenticated = ({ children }) => {
 
+    const { isLoggedIn } = useContext(AuthContext);
 
-
+    return (
+        <div>
+            {isLoggedIn ? children : (
+                <div className='authenticated-hoc'>
+                    <h2>You need to be logged in to access this content.</h2>
+                </div>
+            )}
+        </div>
+    )
 };
 
 export default Authenticated;
