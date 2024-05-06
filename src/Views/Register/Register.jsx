2c44f2aa8893c91b18eb80eb0620907611b21a2d
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-05-06 23:55:56 +0300

    refactored the Register.jsx to add spinning circle and useEffect(); added CSS layout as well.

--- src/Views/Register/Register.jsx
+++ src/Views/Register/Register.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { checkIfUserExists, createUser } from '../../service/request-service.js';
 import { registerUser } from '../../service/authentication-service.js';
@@ -7,71 +7,97 @@ import './Register.css';
 import { MIN_CHAR_LENGTH, MAX_CHAR_LENGTH, EMAIL_REGEX, DIGIT_REGEX, LETTER_REGEX, ALPHA_NUMERIC_REGEX, SPECIAL_CHARS_REGEX } from '../../common/constants.js';
 
 const Register = () => {
-
     const [userExists, setUserExists] = useState(false);
     const [isRegSuccessful, setIsRegSuccessful] = useState(false);
-    const [count, setCount] = useState(5);
+    const [count, setCount] = useState(7);
+    const [form, setForm] = useState({
+        firstName: '',
+        lastName: '',
+        emailAddress: '',
+        username: '',
+        password: ''
+    });
+    const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+    const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
     const { setLoginState } = useContext(AuthContext);
 
-    const getFormData = () => {
-        const firstName = document.querySelector('#first-name').value;
-        const lastName = document.querySelector('#last-name').value;
-        const emailAddress = document.querySelector('#email').value;
-        const username = document.querySelector('#username').value;
-        const password = document.querySelector('#password').value;
+    useEffect(() => {
+        if (isFormSubmitted) {
+            const registrationHandler = async () => {
+                try {
+                    setLoading(true);
+                    const snapshot = await checkIfUserExists(form.username);
+                    if (snapshot.exists()) {
+                        setLoading(false);
+                        setUserExists(true);
+                        return;
+                    }
 
-        return { firstName, lastName, emailAddress, username, password }
-    }
+                    const userCredentials = await registerUser(form.emailAddress, form.password);
+                    if (typeof userCredentials === 'string' && userCredentials.includes('auth/email-already-in-use')) {
+                        throw new Error(`${form.emailAddress} email address is already in use.`);
+                    }
 
-    const register = async (event) => {
-        event.preventDefault();
+                    const creationStatus = await createUser({
+                        firstName: form.firstName, 
+                        lastName: form.lastName, 
+                        emailAddress: form.emailAddress, 
+                        username: form.username, 
+                        password: form.password,
+                    });
 
-        const formData = getFormData();
+                    if (!creationStatus) {
+                        setLoading(false);
+                        setIsRegSuccessful(true);
+                    }
 
-        if (formData.firstName.length < MIN_CHAR_LENGTH || formData.firstName.length > MAX_CHAR_LENGTH) {
+                } catch (error) {
+                    setLoading(false);
+                    alert(error.message);
+                }
+            }
+            registrationHandler();
+        }
+    }, [form]);
+
+    const register = (event) => {
+        event.preventDefault();        
+        const firstName = event.target.firstName.value;
+        const lastName = event.target.lastName.value;
+        const emailAddress = event.target.email.value;
+        const username = event.target.username.value;
+        const password = event.target.password.value;
+
+        if (firstName.length < MIN_CHAR_LENGTH || firstName.length > MAX_CHAR_LENGTH) {
             alert(`First name must be between ${MIN_CHAR_LENGTH} and ${MAX_CHAR_LENGTH} characters long.`); 
             return;
         }
 
-        if (formData.lastName.length < MIN_CHAR_LENGTH || formData.lastName.length > MAX_CHAR_LENGTH) {
+        if (lastName.length < MIN_CHAR_LENGTH || lastName.length > MAX_CHAR_LENGTH) {
             alert(`Last name must be between ${MIN_CHAR_LENGTH} and ${MAX_CHAR_LENGTH} characters long.`); 
             return;
         }
 
-        if (!EMAIL_REGEX.test(formData.emailAddress)) {
-            alert(`${formData.emailAddress} is not a valid email address.`);
+        if (!EMAIL_REGEX.test(emailAddress)) {
+            alert(`${emailAddress} is not a valid email address.`);
             return;
         }
 
-        if (formData.username.length < 8 || !ALPHA_NUMERIC_REGEX.test(formData.username) 
-            || !DIGIT_REGEX.test(formData.username) || !LETTER_REGEX.test(formData.username)) {
-                alert(`${formData.username} is not a valid username.`);
+        if (username.length < 8 || !ALPHA_NUMERIC_REGEX.test(username) 
+            || !DIGIT_REGEX.test(username) || !LETTER_REGEX.test(username)) {
+                alert(`${username} is not a valid username.`);
                 return;
         }
 
-        if (formData.password.length < 8 || !SPECIAL_CHARS_REGEX.test(formData.password) 
-            || !DIGIT_REGEX.test(formData.password) || !LETTER_REGEX.test(formData.password) ) {
-                alert(`${formData.password} is not a valid password.`);
+        if (password.length < 8 || !SPECIAL_CHARS_REGEX.test(password) 
+            || !DIGIT_REGEX.test(password) || !LETTER_REGEX.test(password) ) {
+                alert(`${password} is not a valid password.`);
                 return;
         }
 
-        const snapshot = await checkIfUserExists(formData.username);
-        if (snapshot.exists()) {
-            setUserExists(true);
-            return;
-        }
-
-        const userCredentials = await registerUser(formData.emailAddress, formData.password);
-        if (typeof userCredentials === 'string' && userCredentials.includes('auth/email-already-in-use')) {
-            alert(`${formData.emailAddress} email address is already in use.`);
-            return;
-        }
-
-        const creationStatus = await createUser(formData);
-        if (!creationStatus) {
-            setIsRegSuccessful(true);
-        }
+        setForm({ firstName, lastName, emailAddress, username, password });
+        setIsFormSubmitted(true);
     }
 
     const countDown = () => {
@@ -80,6 +106,12 @@ const Register = () => {
             navigate('/');
         }
         else setTimeout(() => setCount(count - 1), 1000);
+    }
+
+    if (loading) {
+        return (
+            <div className='spinner'></div>
+        )
     }
     
     if (userExists) {
@@ -105,9 +137,9 @@ const Register = () => {
 
     return (
         <form onSubmit={register} className="register-form">
-            <span><label htmlFor="first-name">First Name </label><input type="text" name="first-name" id='first-name' required /></span> <br />
+            <span><label htmlFor="firstName">First Name </label><input type="text" name="firstName" id='firstName' required /></span> <br />
             <h5><i>/ First name must be between 4 and 32 characters long. /</i></h5> <br />
-            <span><label htmlFor="last-name">Last Name </label><input type="text" name="last-name" id='last-name' required /></span> <br />
+            <span><label htmlFor="lastName">Last Name </label><input type="text" name="lastName" id='lastName' required /></span> <br />
             <h5><i>/ Last name must be between 4 and 32 characters long. /</i></h5> <br />
             <span><label htmlFor="email">Email address </label><input type="email" name="email" id='email' required /></span> <br />
             <span><label htmlFor="username">Username </label><input type="text" name="username" id='username' required /></span> <br />
