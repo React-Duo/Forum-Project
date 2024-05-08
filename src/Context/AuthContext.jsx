3936c6f392639de3600545e278b86a0957a2be17
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-05-08 15:59:42 +0300

    removed unnecessary default context from AuthContext.jsx (as it is provided in App (main) component

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -1,8 +1,5 @@
 import { createContext } from "react";
 
-const AuthContext = createContext({
-    isLoggedIn: false,
-    setLoginState: () => {}
-});
+const AuthContext = createContext();
 
 export default AuthContext;
