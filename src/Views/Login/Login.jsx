2c75b5e53c6e9088a0d43e1a01d24f4e09f20879
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-05-08 17:07:27 +0300

    fixed incorrectly used form field -> form.emailAddress in Login.jsx

--- src/Views/Login/Login.jsx
+++ src/Views/Login/Login.jsx
@@ -41,7 +41,7 @@ const Login = () => {
     useEffect(() => {
         if (isLoginSuccessful) {
             if (count === 0) {
-                setLoginState({status: true, user: form.username});
+                setLoginState({status: true, user: form.emailAddress});
                 navigate('/');
             }
             else setTimeout(() => setCount(count - 1), 1000);
