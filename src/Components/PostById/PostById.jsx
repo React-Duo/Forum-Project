6bb38d13a41508af76ceca90ada0a12cb906f4ad
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-05-08 22:09:44 +0300

    implemented PostById.jsx

--- src/Components/PostById/PostById.jsx
+++ src/Components/PostById/PostById.jsx
@@ -1,39 +1,71 @@
-import React, { useContext } from "react";
-import "./PostById.css";
-import { assets } from "../../assets/assets";
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from 'react-router-dom';
 import AuthContext from '../../Context/AuthContext';
+import { getPostById } from '../../service/request-service.js';
+import { assets } from "../../assets/assets";
+import "./PostById.css";
 
 const PostById = () => {
-
   const { isLoggedIn, setLoginState } = useContext(AuthContext);
+  const params = useParams();
+  const [post, setPost] = useState(null);
+  const [likes, setLikes] = useState([]);
 
   console.log(isLoggedIn);
 
+  useEffect(() => {
+    const getSinglePost = async () => {
+      const singlePost = await getPostById(params.id);
+      setPost(singlePost);
+    }
+    getSinglePost();
+  }, []);
+
+  useEffect(() => {
+    if (post) {
+      console.log(post);
+      const likes = Object.keys(post.postLikedBy);
+      console.log(likes);
+      setLikes(likes);
+    }
+  }, [post]);
+
+  
+  const viewLikes = (event) => {
+    event.preventDefault();
+
+    console.log(likes);
+
+    alert(likes);
+
+    
+
+
+
+  }
 
   return (
+    post && 
     <div>
       <div className="postById">
         <div className="personDetails">
           <img src={assets.profile}></img>
-          <h4>Pesho</h4>
+          <h4>{post.postAuthor}</h4>
         </div>
         <div className="postContent">
-          <h3>Some title</h3>
-          <p>
-            somecontent hert wae wae saed somecontent hert wae wae saedsomecontent hert wae wae saedsomecontent hert wae wae saed
-             hert wae wae saed somecontent hert wae wae saedsomecontent hert wae wae saedsomecontent hert wae wae saed
-          </p>
+          <h3>{post.postTitle}</h3> <br />
+          <p>{post.postContent}</p>
         </div>
         <div className="interactions">
           <p>
             <i className="fa-solid fa-thumbs-up fa-lg"></i>
-            22
+            {likes.length && <a href="" onClick={viewLikes}>{likes.length}</a>}
           </p>
           <p>
             <i className="fa-solid fa-comment fa-lg"></i>
             3
           </p>
-          <p>May 5 2024</p>
+          <p>{post.date}</p>
         </div>
       </div>
     </div>
