cd4c154d1276cb3835d13cf2feed18cf4e7df107
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-05-09 17:40:21 +0300

    implemented AllComments.jsx component (basic)

--- src/Components/AllComments/AllComments.jsx
+++ src/Components/AllComments/AllComments.jsx
@@ -2,37 +2,33 @@ import React from "react";
 import "./AllComments.css";
 import { assets } from "../../assets/assets";
 
-const AllComments = () => {
+const AllComments = (props) => {
+
   return (
     <div className="allComments">
     <h5>All comments</h5>
-      <div className="comment">
-        <div className="personDetails">
-          <img src={assets.profile}></img>
-          <h4>Pesho</h4>
-        </div>
-        <div className="commentContent">
+    {props.comments && props.comments.map(comment => {
+      console.log(comment);
+      return <div className="comment">
+          <div className="personDetails">
+            <img src={assets.profile}></img>
+            <h4>{comment.commentAuthor}</h4>
+          </div>
+          <div className="commentContent">
+              <p>{comment.commentContent}</p>
+          </div>
+          <div className="interactions">
             <p>
-                Somecontent hert wae wae saed somecontent hert wae wae saedsomecontent hert wae wae saedsomecontent hert wae wae saed
-                hert wae wae saed somecontent hert wae wae saedsomecontent hert wae wae saedsomecontent hert wae wae saed
+              <i className="fa-solid fa-thumbs-up fa-lg"></i>
+            {Object.keys(comment.commentLikedBy).length}
             </p>
-        </div>
-        <div className="interactions">
-          <p>
-            <i className="fa-solid fa-thumbs-up fa-lg"></i>
-            22
-          </p>
-          <p>
-            <i className="fa-solid fa-comment fa-lg"></i>
-            3
-          </p>
-          <p>May 5 2024</p>
-        </div>
-        <hr/>
+            <p>{comment.date}</p>
+          </div>
+          <hr/>
       </div>
-      
+    })}
     </div>
-  );
+  )
 };
 
 export default AllComments;
