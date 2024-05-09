cf3b1e728d6ae54be8e09be21c02d121debcaf9c
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-05-09 20:13:40 +0300

    removed the call for AllComments component from SinglePost.jsx as it is called in PostById.jsx now

--- src/Views/SinglePost/SinglePost.jsx
+++ src/Views/SinglePost/SinglePost.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import PostById from "../../Components/PostById/PostById";
 import "./SinglePost.css";
-import AllComments from "../../Components/AllComments/AllComments";
 import AddComment from "../../Components/AddComment/AddComment";
 
 const SinglePost = () => {
   return (
     <div className="singlePostContainer">
       <PostById />
-      <AllComments />
       <AddComment />
     </div>
   );
