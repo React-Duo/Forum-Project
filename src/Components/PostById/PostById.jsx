5e6226708a963156ad1097b00653d09b5a14ff3f
Petko Petkov <petkozlatilov1234@gmail.com>
2024-05-10 15:24:52 +0300

    added option to remove the post if the author is the same like the logged in user

--- src/Components/PostById/PostById.jsx
+++ src/Components/PostById/PostById.jsx
@@ -1,14 +1,22 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from 'react-router-dom';
-import { getPostById, getCommentsByPost } from '../../service/request-service.js';
-import AllComments from '../AllComments/AllComments.jsx';
+import React, { useEffect, useState, useContext } from "react";
+import { useParams } from "react-router-dom";
+import {
+  getPostById,
+  getCommentsByPost,
+  getUsers,
+  removePost,
+} from "../../service/request-service.js";
+import AllComments from "../AllComments/AllComments.jsx";
 import { assets } from "../../assets/assets";
 import "./PostById.css";
+import AuthContext from "../../Context/AuthContext.jsx";
 
 const PostById = () => {
   const params = useParams();
   const postId = params.id;
   const [post, setPost] = useState(null);
+  const [user, setUser] = useState();
+  const { isLoggedIn, setLoginState } = useContext(AuthContext);
 
   useEffect(() => {
     const getSinglePost = async () => {
@@ -20,34 +28,72 @@ const PostById = () => {
       setPost({postAuthor, postTitle, postContent, date, likes, comments});
     }
     getSinglePost();
-  }, []);
+
+    const fetchUsers = async () => {
+      let users = await getUsers();
+      users = Object.entries(users);
+      if (isLoggedIn.status) {
+        const currentUsername = users.filter(
+          (user) => user[1].emailAddress === isLoggedIn.user
+        )[0][0];
+        setUser(currentUsername);
+      }
+    };
+    fetchUsers();
+  }, [user]);
+
+  const [showOptions, setShowOptions] = useState(false);
+
+  const handleEditOptions = () => {
+    setShowOptions(!showOptions);
+  };
 
   return (
-    post && 
-    <div>
-      <div className="postById">
-        <div className="personDetails">
-          <img src={assets.profile}></img>
-          <h4>{post.postAuthor}</h4>
+    post && (
+      <div>
+        <div className="postById">
+          <div className="personDetails">
+            <img src={assets.profile}></img>
+            <h4>{post.postAuthor}</h4>
+          </div>
+          <div className="postContent">
+            <h3>{post.postTitle}</h3> <br />
+            <p>{post.postContent}</p>
+          </div>
+          <div className="interactions">
+            <p>
+              <i className="fa-solid fa-thumbs-up fa-lg"></i>
+              {post.likes.length}
+            </p>
+            <p>
+              <i className="fa-solid fa-comment fa-lg"></i>
+              {post.comments.length}
+            </p>
+            <p>{post.date}</p>
+            {post.postAuthor === user ? (
+              <div className="editOptions">
+                <a>
+                  <i className="fa-solid fa-pen-to-square"></i>
+                </a>
+                <a onClick={handleEditOptions}>
+                  <i className="fa-solid fa-trash"></i>
+                </a>
+              </div>
+            ) : (
+              ""
+            )}
+          </div>
         </div>
-        <div className="postContent">
-          <h3>{post.postTitle}</h3> <br />
-          <p>{post.postContent}</p>
-        </div>
-        <div className="interactions">
-          <p>
-            <i className="fa-solid fa-thumbs-up fa-lg"></i>
-           {post.likes.length}
-          </p>
-          <p>
-            <i className="fa-solid fa-comment fa-lg"></i>
-            {post.comments.length}
-          </p>
-          <p>{post.date}</p>
-        </div>
+        {showOptions && (
+          <div className="deleteConfirm">
+            <p>Are you sure you want to delete the post</p>
+            <button onClick={() => removePost(postId)}>Yes</button>
+            <button onClick={handleEditOptions}>No</button>
+          </div>
+        )}
+        <AllComments comments={post.comments} />
       </div>
-      <AllComments comments={post.comments} />
-    </div>
+    )
   );
 };
 
