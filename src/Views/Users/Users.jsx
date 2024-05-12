e71cc2ec0f953b42fe8d86d0a6d779c4c33cd9af
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-05-12 19:58:49 +0300

    Implemented the Users.jsx view and its CSS layout (basic)

--- src/Views/Users/Users.jsx
+++ src/Views/Users/Users.jsx
@@ -1,7 +1,52 @@
+import { useEffect, useState } from 'react';
+import './Users.css';
+import { searchUser } from '../../service/request-service';
+
 const Users = () => {
+    const [searchParams, setSearchParams] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const handleUserSearch = async () => {
+
+
+        await searchUser(searchParams.searchString, searchParams.searchTerm);
+
+    };
+
+    useEffect(() => {if (searchParams) handleUserSearch()}, [searchParams]);
+
+    const handleSearchForm = (event) => {
+        event.preventDefault();
+        const searchString = event.target.searchField.value;
+        const searchTerm = event.target.searchType.value;
+        setSearchParams({ searchString, searchTerm });
+    }
+
+    if (loading) {
+        return <div className="spinner"></div>
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSearchForm} className="search-form">
+                <input type="text" id="searchField" name="searchField" required/> <br />
+                <span>
+                    <input type="radio" value="firstName" id="firstName" name="searchType" required /> 
+                    <label htmlFor="firstName">First Name</label>
+                    <input type="radio" value="lastName" id="lastName" name="searchType" required /> 
+                    <label htmlFor="lastName">Last Name</label>
+                    <input type="radio" value="emailAddress" id="emailAddress" name="searchType" required /> 
+                    <label htmlFor="emailAddress">Email Address</label>
+                    <input type="radio" value="username" id="username" name="searchType" required /> 
+                    <label htmlFor="username">Username</label>
+                </span><br />
+                <button type="submit">Search</button>
+            </form>
 
 
 
+        </div>
+    )
 }
 
 export default Users;
