714cbfe666e9cc997bc7563010927a13afa45473
Petko Petkov <petkozlatilov1234@gmail.com>
2024-05-13 19:43:11 +0300

    added users photos

--- src/Components/AllComments/AllComments.jsx
+++ src/Components/AllComments/AllComments.jsx
@@ -1,11 +1,16 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import "./AllComments.css";
 import { assets } from "../../assets/assets";
-import { getSingleComment, getUsers, updateCommentLikes } from "../../service/request-service";
-import AuthContext from '../../Context/AuthContext';
+import {
+  getSingleComment,
+  getUsers,
+  updateCommentLikes,
+} from "../../service/request-service";
+import AuthContext from "../../Context/AuthContext";
 
 const AllComments = (props) => {
   const { isLoggedIn } = useContext(AuthContext);
+  const [usersDetails, setUserDetails] = useState([]);
 
   const handleLikes = async (commentId) => {
     const users = await getUsers();
@@ -24,30 +29,61 @@ const AllComments = (props) => {
 
     updateCommentLikes(commentId, comment.commentLikedBy);
     props.fn();
-  }
+  };
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      let users = await getUsers();
+      users = Object.entries(users);
+      setUserDetails(users);
+    };
+    fetchUsers();
+  }, [isLoggedIn]);
 
   return (
     <div className="allComments">
       <h5>All comments</h5>
-    {props.comments && (props.comments.length ? props.comments.map((comment) => {
-      return <div className="comment" key={comment.id}>
-          <div className="personDetails">
-            <img src={assets.profile}></img>
-            <h4>{comment.commentAuthor}</h4>
-          </div>
-          <div className="commentContent">
-              <p>{comment.commentContent}</p>
-          </div>
-          <div className="interactions">
-            <p>
-              <i className="fa-solid fa-thumbs-up fa-lg" onClick={() => handleLikes(comment.id)}></i>
-            { comment.commentLikedBy ? Object.keys(comment.commentLikedBy).length : 0 }
-            </p>
-            <p>{comment.date}</p>
-          </div>
-          <hr/>
-      </div>
-    }) : <h4>No Comments added yet.</h4>)}
+      {props.comments &&
+        (props.comments.length ? (
+          props.comments.map((comment) => {
+            let currentUsername;
+            if (usersDetails.length > 0) {
+              currentUsername = usersDetails.filter(
+                (el) => el[1]?.username === comment?.commentAuthor
+              )[0][1];
+            }        
+            return (
+              <div className="comment" key={comment.id}>
+                <div className="personDetails">
+                  <img
+                    src={
+                      currentUsername ? currentUsername.photo : assets.profile
+                    }
+                  ></img>
+                  <h4>{comment.commentAuthor}</h4>
+                </div>
+                <div className="commentContent">
+                  <p>{comment.commentContent}</p>
+                </div>
+                <div className="interactions">
+                  <p>
+                    <i
+                      className="fa-solid fa-thumbs-up fa-lg"
+                      onClick={() => handleLikes(comment.id)}
+                    ></i>
+                    {comment.commentLikedBy
+                      ? Object.keys(comment.commentLikedBy).length
+                      : 0}
+                  </p>
+                  <p>{comment.date}</p>
+                </div>
+                <hr />
+              </div>
+            );
+          })
+        ) : (
+          <h4>No Comments added yet.</h4>
+        ))}
     </div>
   )
 };
